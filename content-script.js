10a744649664c3fc1e884ef4cee4ee2edfa4330e
MadLadMadara <s.aj.mcruvie@gmail.com>
2024-06-24 10:02:18 +0100

    Extracted selected text nodes

--- content-script.js
+++ content-script.js
@@ -1,13 +1,40 @@
-(async () => {
-    // main body, required for top level await of imports
-    console.log("content-script.js loaded");
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        switch (request.action) {
-            case "get_dom_text":
-                sendResponse(document.all[0].outerHTML);
-                break;
-            default:
-                break;
+/*
+    Url:https://stackoverflow.com/questions/70534844/getting-all-the-involved-nodes-of-a-selection-in-javascript
+    "I've spent a day and a half on this 
+    (it's over a hundred lines of code in three separate functions);
+    each time I think I'm done I find another edge case that break the whole thing. 
+    Attempts to refactor are a mess. 
+    Adding to this, it's not clear which end is inclusive" 
+    – Michael November 20, 2022 at 3:17, StackOverflow
+
+    "We choose to go to the Moon in this decade and do the other things, 
+    not because they are easy, but because they are hard." 
+    - JFK September 12, 1962, Rice University, Houston, Texas
+*/
+const context = 'content-script';
+console.log(`${context}:loaded at:${new Date().toLocaleTimeString()}`); 
+// TODO: Handle request from background script, to read the selected text
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    switch (message.message) {
+        case "startReading": {
+            let selection = window.getSelection();
+            console.log(`${context}:start reading:${selection.toString()}`);
+            let rne = new SelectionTextNodeExtractor(selection);
+            rne.getNodes().forEach(node => {
+                console.group();
+                console.log(`${context}:node name:${node.nodeName}`); 
+                console.log(`${context}:node text:${node.textContent}`);
+                console.log(`${context}:node tag:${node.tagName}`);
+                console.groupEnd();
+            });
         }
-    });
-})();
+        break;
+        case "stopReading": {
+            console.log(`${context}:stop reading:${message.message}`);
+        }
+        break;
+        default: {
+            console.log(`${context}:unknown message:${message.message}`);
+        }
+    }
+});
--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,23 @@
-// imports and local variables
-
-import Readability from "/node_modules/@mozilla/readability/Readability.js";
-import isProbablyReaderable from "/node_modules/@mozilla/readability/Readability-readerable.js";
-import {JSDOM} from '/lib/jsdom.js';         
-
-// required for updating/installing content scripts
-const manifestData = chrome.runtime.getManifest();
-// var { Readability } = require('@mozilla/readability');
-// var { JSDOM } = require('jsdom');
-// var doc = new JSDOM("<body>Look at this cat: <img src='./cat.jpg'></body>", {
-//   url: "https://www.example.com/the-page-i-got-the-source-from"
-// });
-// let reader = new Readability(doc.window.document);
-// let article = reader.parse();
+const context = 'service-worker';
+console.log(`${context}:loaded at:${new Date().toLocaleTimeString()}`); 
 
 
-// on install
-chrome.runtime.onInstalled.addListener((details) => {
-    // create context menu
+chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
-        id: "openSidePanel",
-        title: "Open TTS Highlighter Side Panel",
-        contexts: ["all"],
+        id: "readSelectedText",
+        title: "Read selected text", 
+        contexts:["selection"], 
     });
-    console.log("TTS Highlighter installed");
 });
 
-// on context menu click events
-chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId === "openSidePanel") {
-        chrome.sidePanel.open({ windowId: tab.windowId });
-    }
-});
 
-// chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-//     if (info.menuItemId === "openSidePanel") {
-//         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//             chrome.tabs.sendMessage(
-//                 tabs[0].id,
-//                 {action: "get_dom_text"},
-//                 (response) => {
-//                     const document = new JSDOM(response, {
-//                         url: tabs[0].url,
-//                         referrer: tabs[0].url,
-//                         contentType: "text/html",
-//                         includeNodeLocations: true,
-//                     }).window.document;
-//                     if (isProbablyReaderable(document)) {
-//                         console.log("This page is readerable");
-//                     } else {
-//                         console.log("This page is not readerable");
-//                     }
-//                     chrome.sidePanel.open({ windowId: tab.windowId });
-
-//                     const reader = new Readability(document).parse();
-//                     console.group("Readability");
-//                     console.log(`Title: ${reader.title}`);
-//                     console.log(`Byline: ${reader.byline}`);
-//                     console.log(`Content: ${reader.content}`);
-//                     console.log(`Text Content: ${reader.textContent}`);
-//                     console.log(`Length: ${reader.length}`);
-//                     console.log(`Excerpt: ${reader.excerpt}`);
-//                     console.log(`Site Name: ${reader.siteName}`);
-//                     console.log(`Direction: ${reader.dir}`);
-//                     console.log(`Language: ${reader.lang}`);
-//                     console.groupEnd();
-//                 }
-//             );
-//         });
-//     }
-// });
-
+chrome.contextMenus.onClicked.addListener((info) => {
+    chrome.tabs.query({active : true, lastFocusedWindow : true}, function (tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        if (info.menuItemId === 'readSelectedText') {
+            console.log(`${context}:read selected text from:${url}`);
+            chrome.tabs.sendMessage(tab.id, {message: "startReading"});
+        }
+    });
+  })
