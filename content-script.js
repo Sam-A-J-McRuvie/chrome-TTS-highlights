545ba201dd73b84cb46f6af8dd56aeaf5a9cd484
Max-McRuvie <mcruvie@hotmail.co.uk>
2024-07-10 17:45:33 +0100

    renamed SelectTextExtractor class

--- content-script.js
+++ content-script.js
@@ -24,7 +24,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
 
             console.log(`${context}:start reading: \n ${selection.toString()}`);
-            let ste = new SelectTextExtractor(selection);
+            let ste = new SelectedTextExtractor(selection);
 
 
             console.group();
